5c60cd652403ce03568e044155aacecb7d5454f1
James Keen <jpkeen@valdosta.edu>
2014-03-29 01:32:45 -0400

    Added user entries for seed file. Added some divs from css for users. Other minor tweakin, nothing to write home about.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,47 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # user = User.first 
 user = User.create!(
-  email: 'jpkeen1@gmail.com',
+  email: 'jpkeen@valdosta.edu',
   password: 'password',
   password_confirmation: 'password'
 )
+
+user = User.create!(
+  email: 'leajones@valdosta.edu',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+user = User.create!(
+  email: 'johnmjones@valdosta.edu',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+user = User.create!(
+  email: 'pdnguyen@valdosta.edu',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+user = User.create!(
+  email: 'tjwoods@valdosta.edu',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+user = User.create!(
+  email: 'admin@admin.com',
+  password: 'adminadmin',
+  password_confirmation: 'adminadmin'
+)
+
+user = User.create!(
+  email: 'admin@admin.com',
+  password: 'adminadmin',
+  password_confirmation: 'adminadmin'
+)
+
 
 forum = Forum.create!(topic: 'I am a Forum', user: user)
 ForumsPost.create!(description: 'I am a Post', user: user, forum: forum)
