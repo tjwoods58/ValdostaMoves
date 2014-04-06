e58c996ab25653d31f1f147397ececb350fad703
John M. Jones <johnmjones@valdosta.edu>
2014-04-06 03:04:41 -0400

    Getting closer to events...

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,15 @@
 class Event < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
+  def index
+  #gets list
+
+    @events = Event.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @events }
+    end
+  end
 end
 
