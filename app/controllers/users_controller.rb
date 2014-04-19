3981ee257d8e1daf9ed01d7099abd22490f05be6
Phuong Nguyen <pdnguyen@valdosta.edu>
2014-04-18 23:15:10 -0400

    fix user edit and destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,62 +6,59 @@ class UsersController < ApplicationController
   end
 
   def edit
-    is_user
+
   end
 
-  def create
-    @user = User.new(params[:user])
-    @user_session = UserSession.new(params[:user])
+  #def create
+   # @user = User.new(params[:user])
+    #@user_session = UserSession.new(params[:user])
  
-    respond_to do |format|
-      if @user.save && @user_session.save
-        format.html { redirect_to(home_index, :notice => 'Registration successfull.') }
-      else
-        format.html { render :action => "new" }
-      end
-    end
-  end
+    #respond_to do |format|
+     # if @user.save && @user_session.save
+      #  format.html { redirect_to(home_index, :notice => 'Registration successfull.') }
+      #else
+        
+      #end
+    #end
+  #end
   
   
   def update
-    is_user
-    respond_to do |format|
     if @user.update(user_params)
       redirect_to user_path @user
     else
       render action: 'edit'
     end
-    end
   end
 
-  def new    
-    @user = User.new
+  #def new    
+   # @user = User.new
     
-    if current_user
-      redirect_to(home_index, :notice => 'Already registered')
-    end
-  end
+    #if current_user
+     # redirect_to(home_index, :notice => 'Already registered')
+    #end
+  #end
   
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def destroy
-    clear_facebook_session_information
+    #clear_facebook_session_information
     @user.destroy
     redirect_to users_path
   end 
 
-  def is_user
-    if User.exists?(params[:id])
-      @user = User.find(params[:id]);
-      if !current_user || current_user.id != @user.id
-        redirect_to(home_index, :notice =>"You do not have access to that page")
-      end
-    else
-      redirect_to(home_index, :notice =>"You do not have access to that page")
-    end
-  end
+  #def is_user
+   # if User.exists?(params[:id])
+    #  @user = User.find(params[:id]);
+     # if !current_user || current_user.id != @user.id
+      #  redirect_to(home_index_path, :notice =>"You do not have access to that page")
+     # end
+    #else
+     # redirect_to(home_index_path, :notice =>"You do not have access to that page")
+    #end
+  #end
   
   private
     def user_params
