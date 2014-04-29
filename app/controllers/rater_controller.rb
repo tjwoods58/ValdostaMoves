a295447cfed736f259ddca27364de71bdc581ad5
Terrance Woods <tjwoods58@gmail.com>
2014-04-29 00:19:33 -0400

    Added new buttons on home page to show upcoming events and twitter feed only when you click on it. Added css to events so comments and ratins are together. Added css to activities page. Changed when users rate from getting 1 point to 4 points.

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -4,7 +4,7 @@ class RaterController < ApplicationController
     if user_signed_in?
       obj = params[:klass].classify.constantize.find(params[:id])
       obj.rate params[:score].to_i, current_user, params[:dimension]
-      current_user.event_points += 1
+      current_user.event_points += 4
       current_user.update_attributes(:event_points => current_user.event_points)
 
       render :json => true
