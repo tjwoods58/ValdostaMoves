660f1693759000df08ab013d599e5a0f9ba803ab
matthew7j <johnmjones@valdosta.edu>
2014-04-30 19:15:09 -0700

    Got seed file for comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,6 +69,10 @@ Event.create!(
   contactphone: 1234567891,
 )
 
+
+
+
+
 User.delete_all
 user = User.create!(
   email: 'jpkeen@valdosta.edu',
@@ -134,3 +138,397 @@ user = User.create!(
 forum = Forum.create!(title: 'I am a Forum', user: user)
 topic = Topic.create!(title: 'I am a Topic', user: user, forum:forum)
 post = Post.create!(body: 'I am a Post', user: user, topic: topic)
+
+Comment.delete_all
+Comment.create!(
+  user_id: 1,
+  event_id: 1,
+  comment: 'I cannot wait to go to this event. I went to the last one and it was a lot of fun!',
+  created_at: '2014-04-17 17:00:00.000000',
+  updated_at: '2014-04-17 17:00:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 1,
+  comment: 'I will be attending as well, see you there James!',
+  created_at: '2014-04-17 17:12:00.000000',
+  updated_at: '2014-04-17 17:12:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 1,
+  comment: 'I will be out there as well, looking forward to seeing you guys there',
+  created_at: '2014-04-17 17:24:00.000000',
+  updated_at: '2014-04-17 17:24:00.000000'
+)
+Comment.create!(
+  user_id: 4,
+  event_id: 1,
+  comment: 'I will not be able to make it guys, have fun!',
+  created_at: '2014-04-17 18:05:00.000000',
+  updated_at: '2014-04-17 18:05:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 1,
+  comment: 'I am going as well, cannot wait to see you guys there <3',
+  created_at: '2014-04-17 19:00:00.000000',
+  updated_at: '2014-04-17 19:00:00.000000'
+)
+Comment.create!(
+  user_id: 1,
+  event_id: 1,
+  comment: 'That was a lot of fun, I am glad that I went!',
+  created_at: '2014-04-19 12:01:00.000000',
+  updated_at: '2014-04-19 12:01:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 1,
+  comment: 'I had fun as well, thanks for coming everyone!',
+  created_at: '2014-04-21 15:44:00.000000',
+  updated_at: '2014-04-21 15:44:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 1,
+  comment: 'Same here. I gave this event a 5 rating because of how much I liked it! Be sure to like it guys so that they will put together more events like this for us!',
+  created_at: '2014-04-22 16:56:00.000000',
+  updated_at: '2014-04-22 16:56:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 1,
+  comment: 'Yes, I also rated it. Cannot wait for the next one!',
+  created_at: '2014-04-24 12:44:00.000000',
+  updated_at: '2014-04-24 12:44:00.000000'
+)
+
+
+Comment.create!(
+  user_id: 1,
+  event_id: 2,
+  comment: 'I cannot wait to go to this event. I went to the last one and it was a lot of fun!',
+  created_at: '2014-04-17 17:00:00.000000',
+  updated_at: '2014-04-17 17:00:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 2,
+  comment: 'I will be attending as well, see you there James!',
+  created_at: '2014-04-17 17:12:00.000000',
+  updated_at: '2014-04-17 17:12:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 2,
+  comment: 'I will be out there as well, looking forward to seeing you guys there',
+  created_at: '2014-04-17 17:24:00.000000',
+  updated_at: '2014-04-17 17:24:00.000000'
+)
+Comment.create!(
+  user_id: 4,
+  event_id: 2,
+  comment: 'I will not be able to make it guys, have fun!',
+  created_at: '2014-04-17 18:05:00.000000',
+  updated_at: '2014-04-17 18:05:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 2,
+  comment: 'I am going as well, cannot wait to see you guys there <3',
+  created_at: '2014-04-17 19:00:00.000000',
+  updated_at: '2014-04-17 19:00:00.000000'
+)
+Comment.create!(
+  user_id: 1,
+  event_id: 2,
+  comment: 'That was a lot of fun, I am glad that I went!',
+  created_at: '2014-04-29 12:01:00.000000',
+  updated_at: '2014-04-29 12:01:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 2,
+  comment: 'I had fun as well, thanks for coming everyone!',
+  created_at: '2014-04-29 15:44:00.000000',
+  updated_at: '2014-04-29 15:44:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 2,
+  comment: 'Same here. I gave this event a 5 rating because of how much I liked it! Be sure to like it guys so that they will put together more events like this for us!',
+  created_at: '2014-04-30 16:56:00.000000',
+  updated_at: '2014-04-30 16:56:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 2,
+  comment: 'Yes, I also rated it. Cannot wait for the next one!',
+  created_at: '2014-05-02 12:44:00.000000',
+  updated_at: '2014-05-02 12:44:00.000000'
+)
+
+
+Comment.create!(
+  user_id: 1,
+  event_id: 3,
+  comment: 'I cannot wait to go to this event. I went to the last one and it was a lot of fun!',
+  created_at: '2014-04-17 17:00:00.000000',
+  updated_at: '2014-04-17 17:00:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 3,
+  comment: 'I will be attending as well, see you there James!',
+  created_at: '2014-04-17 17:12:00.000000',
+  updated_at: '2014-04-17 17:12:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 3,
+  comment: 'I will be out there as well, looking forward to seeing you guys there',
+  created_at: '2014-04-17 17:24:00.000000',
+  updated_at: '2014-04-17 17:24:00.000000'
+)
+Comment.create!(
+  user_id: 4,
+  event_id: 3,
+  comment: 'I will not be able to make it guys, have fun!',
+  created_at: '2014-04-17 18:05:00.000000',
+  updated_at: '2014-04-17 18:05:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 3,
+  comment: 'I am going as well, cannot wait to see you guys there <3',
+  created_at: '2014-04-17 19:00:00.000000',
+  updated_at: '2014-04-17 19:00:00.000000'
+)
+Comment.create!(
+  user_id: 1,
+  event_id: 3,
+  comment: 'That was a lot of fun, I am glad that I went!',
+  created_at: '2014-04-23 12:01:00.000000',
+  updated_at: '2014-04-23 12:01:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 3,
+  comment: 'I had fun as well, thanks for coming everyone!',
+  created_at: '2014-04-24 15:44:00.000000',
+  updated_at: '2014-04-24 15:44:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 3,
+  comment: 'Same here. I gave this event a 5 rating because of how much I liked it! Be sure to like it guys so that they will put together more events like this for us!',
+  created_at: '2014-04-24 16:56:00.000000',
+  updated_at: '2014-04-24 16:56:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 3,
+  comment: 'Yes, I also rated it. Cannot wait for the next one!',
+  created_at: '2014-04-25 12:44:00.000000',
+  updated_at: '2014-04-25 12:44:00.000000'
+)
+
+
+
+Comment.create!(
+  user_id: 1,
+  event_id: 4,
+  comment: 'I cannot wait to go to this event. I went to the last one and it was a lot of fun!',
+  created_at: '2014-04-17 17:00:00.000000',
+  updated_at: '2014-04-17 17:00:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 4,
+  comment: 'I will be attending as well, see you there James!',
+  created_at: '2014-04-17 17:12:00.000000',
+  updated_at: '2014-04-17 17:12:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 4,
+  comment: 'I will be out there as well, looking forward to seeing you guys there',
+  created_at: '2014-04-17 17:24:00.000000',
+  updated_at: '2014-04-17 17:24:00.000000'
+)
+Comment.create!(
+  user_id: 4,
+  event_id: 4,
+  comment: 'I will not be able to make it guys, have fun!',
+  created_at: '2014-04-17 18:05:00.000000',
+  updated_at: '2014-04-17 18:05:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 4,
+  comment: 'I am going as well, cannot wait to see you guys there <3',
+  created_at: '2014-04-17 19:00:00.000000',
+  updated_at: '2014-04-17 19:00:00.000000'
+)
+Comment.create!(
+  user_id: 1,
+  event_id: 4,
+  comment: 'That was a lot of fun, I am glad that I went!',
+  created_at: '2014-05-19 12:01:00.000000',
+  updated_at: '2014-05-19 12:01:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 4,
+  comment: 'I had fun as well, thanks for coming everyone!',
+  created_at: '2014-05-21 15:44:00.000000',
+  updated_at: '2014-05-21 15:44:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 4,
+  comment: 'Same here. I gave this event a 5 rating because of how much I liked it! Be sure to like it guys so that they will put together more events like this for us!',
+  created_at: '2014-05-22 16:56:00.000000',
+  updated_at: '2014-05-22 16:56:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 4,
+  comment: 'Yes, I also rated it. Cannot wait for the next one!',
+  created_at: '2014-05-24 12:44:00.000000',
+  updated_at: '2014-05-24 12:44:00.000000'
+)
+
+
+
+Comment.create!(
+  user_id: 1,
+  event_id: 5,
+  comment: 'I cannot wait to go to this event. I went to the last one and it was a lot of fun!',
+  created_at: '2014-04-17 17:00:00.000000',
+  updated_at: '2014-04-17 17:00:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 5,
+  comment: 'I will be attending as well, see you there James!',
+  created_at: '2014-04-17 17:12:00.000000',
+  updated_at: '2014-04-17 17:12:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 5,
+  comment: 'I will be out there as well, looking forward to seeing you guys there',
+  created_at: '2014-04-17 17:24:00.000000',
+  updated_at: '2014-04-17 17:24:00.000000'
+)
+Comment.create!(
+  user_id: 4,
+  event_id: 5,
+  comment: 'I will not be able to make it guys, have fun!',
+  created_at: '2014-04-17 18:05:00.000000',
+  updated_at: '2014-04-17 18:05:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 5,
+  comment: 'I am going as well, cannot wait to see you guys there <3',
+  created_at: '2014-04-17 19:00:00.000000',
+  updated_at: '2014-04-17 19:00:00.000000'
+)
+Comment.create!(
+  user_id: 1,
+  event_id: 5,
+  comment: 'That was a lot of fun, I am glad that I went!',
+  created_at: '2014-05-19 12:01:00.000000',
+  updated_at: '2014-05-19 12:01:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 5,
+  comment: 'I had fun as well, thanks for coming everyone!',
+  created_at: '2014-05-21 15:44:00.000000',
+  updated_at: '2014-05-21 15:44:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 5,
+  comment: 'Same here. I gave this event a 5 rating because of how much I liked it! Be sure to like it guys so that they will put together more events like this for us!',
+  created_at: '2014-05-22 16:56:00.000000',
+  updated_at: '2014-05-22 16:56:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 5,
+  comment: 'Yes, I also rated it. Cannot wait for the next one!',
+  created_at: '2014-05-24 12:44:00.000000',
+  updated_at: '2014-05-24 12:44:00.000000'
+)
+
+
+
+
+Comment.create!(
+  user_id: 1,
+  event_id: 6,
+  comment: 'I cannot wait to go to this event. I went to the last one and it was a lot of fun!',
+  created_at: '2014-04-06 17:00:00.000000',
+  updated_at: '2014-04-06 17:00:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 6,
+  comment: 'I will be attending as well, see you there James!',
+  created_at: '2014-04-06 17:12:00.000000',
+  updated_at: '2014-04-06 17:12:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 6,
+  comment: 'I will be out there as well, looking forward to seeing you guys there',
+  created_at: '2014-04-06 17:24:00.000000',
+  updated_at: '2014-04-06 17:24:00.000000'
+)
+Comment.create!(
+  user_id: 4,
+  event_id: 6,
+  comment: 'I will not be able to make it guys, have fun!',
+  created_at: '2014-04-06 18:05:00.000000',
+  updated_at: '2014-04-06 18:05:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 6,
+  comment: 'I am going as well, cannot wait to see you guys there <3',
+  created_at: '2014-04-06 19:00:00.000000',
+  updated_at: '2014-04-06 19:00:00.000000'
+)
+Comment.create!(
+  user_id: 1,
+  event_id: 6,
+  comment: 'That was a lot of fun, I am glad that I went!',
+  created_at: '2014-04-19 12:01:00.000000',
+  updated_at: '2014-04-19 12:01:00.000000'
+)
+Comment.create!(
+  user_id: 2,
+  event_id: 6,
+  comment: 'I had fun as well, thanks for coming everyone!',
+  created_at: '2014-04-21 15:44:00.000000',
+  updated_at: '2014-04-21 15:44:00.000000'
+)
+Comment.create!(
+  user_id: 3,
+  event_id: 6,
+  comment: 'Same here. I gave this event a 5 rating because of how much I liked it! Be sure to like it guys so that they will put together more events like this for us!',
+  created_at: '2014-04-22 16:56:00.000000',
+  updated_at: '2014-04-22 16:56:00.000000'
+)
+Comment.create!(
+  user_id: 5,
+  event_id: 6,
+  comment: 'Yes, I also rated it. Cannot wait for the next one!',
+  created_at: '2014-04-24 12:44:00.000000',
+  updated_at: '2014-04-24 12:44:00.000000'
+)
