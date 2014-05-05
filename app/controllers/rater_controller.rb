fc52f16c9b29c0066cf77ea2a92ecbdddb21ff15
Terrance Woods <tjwoods58@gmail.com>
2014-05-05 10:37:10 -0400

    Changed how points would get added even when you rated a recipe. Fixed the issue of having to reload for stars to show up for show button on events. But not when you press calender. Changed how recipe show page looked because the ingredients and instructions weren't wide enough to really read.

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -4,9 +4,12 @@ class RaterController < ApplicationController
     if user_signed_in?
       obj = params[:klass].classify.constantize.find(params[:id])
       obj.rate params[:score].to_i, current_user, params[:dimension]
-      if rateable_type == Event
+      if params[:dimension] == "rating"
       current_user.event_points += 4
       current_user.update_attributes(:event_points => current_user.event_points)
+      else
+        current_user.event_points += 0.5
+        current_user.update_attributes(:event_points => current_user.event_points)
       end
 
       render :json => true
